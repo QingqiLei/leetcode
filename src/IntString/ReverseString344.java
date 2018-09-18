73321729837eaaf7aeb6d5932bd8c19a07582ccb
bomb <qingqi_lei@icloud.com>
2018-09-18 14:06:54 +0800

    just reverse String

---
+++ src/IntString/ReverseString344.java
@@ -0,0 +1,13 @@
+package IntString;
+
+/**
+ * Write a function that takes a string as input and returns the string reversed.
+ */
+public class ReverseString344 {
+    public String reverseString(String s){
+        char[] result = s.toCharArray();
+        for(int i = result.length -1, j = 0; j < result.length; j++,i--)
+            result[j] =s.charAt(i);
+        return new String(result);
+    }
+}
