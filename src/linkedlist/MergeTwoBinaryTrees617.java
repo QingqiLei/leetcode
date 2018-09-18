d535a61c7612fd55cc887c348c02894dce86b423
bomb <qingqi_lei@icloud.com>
2018-09-18 20:02:55 +0800

    merge two tree using recursion

---
+++ src/linkedlist/MergeTwoBinaryTrees617.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+import tree.TreeNode;
+
+/**
+ * Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+ */
+public class MergeTwoBinaryTrees617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+
+        TreeNode node = new TreeNode(t1.val + t2.val);
+        node.left = mergeTrees(t1.left, t2.left);
+        node.right = mergeTrees(t1.right, t1.right);
+        return node;
+
+    }
+}
