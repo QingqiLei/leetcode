a7af892119ffe41b27b0e22460cf658d51b86db0
bomb <qingqi_lei@icloud.com>
2018-09-18 21:19:18 +0800

    verify the number of letter

---
+++ src/IntString/RobotReturntoOrigin657.java
@@ -0,0 +1,10 @@
+package IntString;
+
+public class RobotReturntoOrigin657 {
+    public boolean judgeCircle(String moves){
+        moves = " "+moves + " ";
+        return moves.split("L").length == moves.split("R").length &&
+                moves.split("U").length == moves.split("D").length;
+    }
+
+}
