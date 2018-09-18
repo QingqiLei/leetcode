9a5cb69325653200dbf983404cd3c45fe12323a2
bomb <qingqi_lei@icloud.com>
2018-09-18 17:57:42 +0800

    regex and Stream

---
+++ src/IntString/KeyboardRow500.java
@@ -0,0 +1,11 @@
+package IntString;
+
+import java.util.stream.Stream;
+
+public class KeyboardRow500 {
+    public String[] findWords(String[] words){
+        String keyBoradRegex = "[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*";
+        return Stream.of(words).filter(word ->word.toLowerCase().matches(keyBoradRegex)).toArray(String[]::new);
+
+    }
+}
