370adf6fd3926aed4d6be03c225f426c554aa9ad
bomb <qingqi_lei@icloud.com>
2018-09-18 15:55:33 +0800

    a little bit of difficult recursive solution

---
+++ src/tree/PathSumIII437.java
@@ -0,0 +1,24 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PathSumIII437 {
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        return helper(root, 0, sum, map);
+
+    }
+
+    private int helper(TreeNode root, int currSum, int target, Map<Integer, Integer> preSum) {
+
+        currSum = currSum+root.val;
+
+        int res = preSum.getOrDefault(currSum -target,0);
+        preSum.put(currSum,preSum.getOrDefault(currSum,0)+1);
+        res += helper(root.left, currSum,target, preSum)+helper(root.right,currSum,target,preSum);
+        preSum.put(currSum,preSum.get(currSum)-1);
+        return res;
+    }
+}
