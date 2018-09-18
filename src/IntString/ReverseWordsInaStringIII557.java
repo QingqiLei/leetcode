3186a33e2b2d754b5fe3d6394facf1bd46e5c019
bomb <qingqi_lei@icloud.com>
2018-09-18 18:05:04 +0800

    StringBuilder reverse()

---
+++ src/IntString/ReverseWordsInaStringIII557.java
@@ -0,0 +1,14 @@
+package IntString;
+
+/**
+ * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+ */
+public class ReverseWordsInaStringIII557 {
+    public String reverseWords(String s) {
+        String[] temp = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < temp.length; i++)
+            result.append(new StringBuffer(temp[i]).reverse().toString() + " ");
+        return result.toString().trim();
+    }
+}
