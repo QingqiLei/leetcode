3e7d1e3ac7befe762238cff024ddcb0cda4e1b05
bomb <qingqi_lei@icloud.com>
2018-09-21 10:32:50 +0800

    simple DFS solution

---
+++ src/dfs/MaxAreaofIsland.java
@@ -0,0 +1,30 @@
+package dfs;
+
+/**
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ *
+ * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ */
+public class MaxAreaofIsland {
+    public static int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1)
+                    maxArea = Math.max(maxArea, areaOfIsland(grid, i, j));
+            }
+        }
+
+        return maxArea;
+    }
+
+    private static int areaOfIsland(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid.length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i, j + 1) + areaOfIsland(grid, i, j - 1);
+        }
+        return 0;
+    }
+
+
+}
