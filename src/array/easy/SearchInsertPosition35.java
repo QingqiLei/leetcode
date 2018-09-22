0dd30d2cc4a5c19284d7561d494ab9c72be3303e
bomb <qingqi_lei@icloud.com>
2018-09-22 18:30:19 +0800

    binary search version

---
+++ src/array/easy/SearchInsertPosition35.java
@@ -0,0 +1,18 @@
+package array.easy;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ */
+
+public class SearchInsertPosition35 {
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (A[mid] == target) return mid;
+            else if (A[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
