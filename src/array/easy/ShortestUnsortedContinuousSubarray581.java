d88c9b702c535c5e347eeb13521e27b68088c21c
bomb <qingqi_lei@icloud.com>
2018-09-22 10:55:24 +0800

    a tricky solution

---
+++ src/array/easy/ShortestUnsortedContinuousSubarray581.java
@@ -0,0 +1,27 @@
+package array.easy;
+
+/**
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+ * <p>
+ * You need to find the shortest such subarray and output its length.
+ * <p>
+ * unsorted order is that the front is bigger than the behind,
+ * SolutionL: so, find the first element that is bigger than the behind
+ * and find the last element that is smaller than the front
+ */
+public class ShortestUnsortedContinuousSubarray581 {
+    public static int findUnsortedSubarray(int[] A) {
+        int n = A.length, beg = -1, end = -2, min = A[n - 1], max = A[0];
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, A[i]);
+            min = Math.min(min, A[n - 1 - i]);
+            if (A[i] < max) end = i;
+            if (A[n - 1 - i] > min) beg = n - 1 - i;
+        }
+        return end - beg + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15}));
+    }
+}
