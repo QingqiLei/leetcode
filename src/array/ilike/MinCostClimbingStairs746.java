664a691089325e481d21087d900bb1bdc42279d7
bomb <qingqi_lei@icloud.com>
2018-09-22 10:56:40 +0800

    dp and greedy solution

--- src/array/ilike/MinCostClimbingStairs746.java
+++ src/array/ilike/MinCostClimbingStairs746.java
@@ -1,5 +1,10 @@
 package array.ilike;
 
+/**
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+ *
+ * Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+ */
 public class MinCostClimbingStairs746 {
     public int minCostClimbingStairs(int[] cost) {
         for (int i = 2; i < cost.length; i++) {
