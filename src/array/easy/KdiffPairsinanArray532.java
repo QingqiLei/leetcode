bc529b061fdfd4b37fcfb49d455ae1e3b7173d52
bomb <qingqi_lei@icloud.com>
2018-09-22 09:41:08 +0800

    two numbers difference is given k in a array

---
+++ src/array/easy/KdiffPairsinanArray532.java
@@ -0,0 +1,29 @@
+package array.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KdiffPairsinanArray532 {
+    public static int findPairs(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0) return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (k == 0) {
+                if (entry.getValue() >= 2)
+                    count++;
+            } else if (map.containsKey(entry.getKey() + k))
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPairs(new int[]{3, 1, 4, 1, 5}, 2));
+        ;
+    }
+}
