fc52c7ba24d2fe9cc6a90468f7814c5e6ff04eec
bomb <qingqi_lei@icloud.com>
2018-09-23 16:41:07 +0800

    new int[26], then count number of each letter

---
+++ src/IntString/RansomNote383.java
@@ -0,0 +1,25 @@
+package IntString;
+
+/**
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+ */
+public class RansomNote383 {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(canConstruct("aaa","aa"));
+        int[] m = new int[1];
+        System.out.println(m[0]);
+    }
+}
