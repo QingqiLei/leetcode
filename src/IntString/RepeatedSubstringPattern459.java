f750c0b2e37eff4cce4db06103300136d40e3e6c
bomb <qingqi_lei@icloud.com>
2018-09-23 20:43:48 +0800

    "easy" solution

---
+++ src/IntString/RepeatedSubstringPattern459.java
@@ -0,0 +1,10 @@
+package IntString;
+
+/**
+ * Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+ */
+public class RepeatedSubstringPattern459 {
+    public boolean repeatedSubstringPattern(String s) {
+        return (s+s).substring(1,s.length()*2 -1).contains(s);
+    }
+}
