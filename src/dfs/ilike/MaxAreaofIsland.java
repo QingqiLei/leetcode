ea0e28f04455e79cd37e645ab59838ed27be93bd
bomb <qingqi_lei@icloud.com>
2018-09-23 23:41:34 +0800

    a easy example of dp problem

--- src/dfs/ilike/MaxAreaofIsland.java
+++ src/dfs/ilike/MaxAreaofIsland.java
@@ -1,8 +1,10 @@
-package dfs;
+package dfs.ilike;
+
+import java.util.Arrays;
 
 /**
  * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
- *
+ * <p>
  * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
  */
 public class MaxAreaofIsland {
@@ -23,6 +25,7 @@ public class MaxAreaofIsland {
             grid[i][j] = 0;
             return 1 + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i, j + 1) + areaOfIsland(grid, i, j - 1);
         }
+
         return 0;
     }
 
