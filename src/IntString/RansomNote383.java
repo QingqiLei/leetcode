fa19c646b362689bf7ec47e2d1ba05d377a14340
bomb <qingqi_lei@icloud.com>
2018-09-23 23:39:36 +0800

    int[] stores the frequence of letter

--- src/IntString/RansomNote383.java
+++ src/IntString/RansomNote383.java
@@ -1,5 +1,7 @@
 package IntString;
 
+import java.math.BigDecimal;
+
 /**
  * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
  */
@@ -14,12 +16,20 @@ public class RansomNote383 {
                 return false;
             }
         }
-        return true;
+return true;
     }
 
     public static void main(String[] args){
         System.out.println(canConstruct("aaa","aa"));
         int[] m = new int[1];
         System.out.println(m[0]);
+        System.out.println("as".indexOf("a"));
+        System.out.println(Integer.valueOf("11",2));
+        Integer int1 = Integer.valueOf("1",2);
+        Integer int2 = Integer.valueOf("11",2);
+        Integer int3 = int1 + int2;
+        System.out.println(Integer.toBinaryString(int3));
+
+
     }
 }
