5512d13f878b07225efd1e6b44f5e2fe9dbddecb
bomb <qingqi_lei@icloud.com>
2018-09-23 16:15:10 +0800

    String regex

---
+++ src/IntString/DetectCapital520.java
@@ -0,0 +1,11 @@
+package IntString;
+
+public class DetectCapital520 {
+    public boolean detectCapitalUse(String word) {
+        return word.matches("[A-Z]|[a-z]|[A-Z][a-z]");
+    }
+
+    public static void main(String[] args){
+        System.out.println("".matches("\\d*"));
+    }
+}
