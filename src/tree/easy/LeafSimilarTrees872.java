af5726f6b6b1070acfb100cbc608ddd14942b6b4
bomb <qingqi_lei@icloud.com>
2018-10-16 21:49:14 +0800

    store the leaf

---
+++ src/tree/easy/LeafSimilarTrees872.java
@@ -0,0 +1,29 @@
+package tree.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafSimilarTrees872 {
+    private List<Integer> list1 = new ArrayList();
+    private List<Integer> list2 = new ArrayList();
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        leafSimilar(root1, list1);
+        leafSimilar(root2, list2);
+
+        return list1.equals(list2);
+
+    }
+
+    void leafSimilar(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return;
+        }
+        leafSimilar(root.left, list);
+        leafSimilar(root.right, list);
+    }
+
+}
