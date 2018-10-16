8c0a041005da9b547487c2b19790718504bd15f9
bomb <qingqi_lei@icloud.com>
2018-10-16 21:08:26 +0800

    the difference of preorder and postorder

---
+++ src/tree/easy/NaryTreePreorderTraversal589.java
@@ -0,0 +1,38 @@
+package tree.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class NaryTreePreorderTraversal589 {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        if (root == null) return list;
+
+        list.add(root.val);
+        for (Node node : root.children)
+            preorder(node);
+
+        return list;
+    }
+
+    public List<Integer> preorder2(Node root) {
+        List<Integer> list = new ArrayList();
+        if (root == null) return list;
+
+
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--)      // oh my facking gosh
+                stack.push(root.children.get(i));
+
+        }
+        return list;
+
+
+    }
+}
