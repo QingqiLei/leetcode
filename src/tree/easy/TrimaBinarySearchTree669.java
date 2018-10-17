56d68b953d29e23fb8136208c8260d2398e87681
bomb <qingqi_lei@icloud.com>
2018-10-17 22:23:51 +0800

    trim binary search tree

---
+++ src/tree/easy/TrimaBinarySearchTree669.java
@@ -0,0 +1,18 @@
+package tree.easy;
+
+/**
+ * trim a binary search tree
+ */
+public class TrimaBinarySearchTree669 {
+    public TreeNode trimBST(TreeNode root, int L, int R){
+
+        if(root.val < L)  return trimBST(root.right,L,R);
+        if(root.val > R)  return trimBST(root.left,L,R);
+
+        root.left = trimBST(root.left, R,L);
+        root.right = trimBST(root.right, R,L);
+
+
+        return root;
+    }
+}
