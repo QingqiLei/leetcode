360047f511e1f4307c1d07da8bb122938d9f1d4b
bomb <qingqi_lei@icloud.com>
2018-10-18 23:41:54 +0800

    BFS  using Queue, first in first out, one layer by one layer

--- src/tree/easy/AverageofLevelsinBinaryTree637.java
+++ src/tree/easy/AverageofLevelsinBinaryTree637.java
@@ -16,7 +16,6 @@ public class AverageofLevelsinBinaryTree637 {
             for(int i = 0; i < n; i ++){
                 if(list.peek().left != null) list.offer(list.peek().left);
                 if(list.peek().right!=null) list.offer(list.peek().right);
-
                 sum = sum + list.poll().val;
             }
             res.add(sum/1.0/n);
