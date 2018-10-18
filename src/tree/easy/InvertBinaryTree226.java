039a060dc892d3a22bbe5994abbcea41d3c701dd
bomb <qingqi_lei@icloud.com>
2018-10-18 23:50:54 +0800

    swap the left and right, recursive, iterative

--- src/tree/easy/InvertBinaryTree226.java
+++ src/tree/easy/InvertBinaryTree226.java
@@ -1,5 +1,10 @@
 package tree.easy;
 
+import java.util.Stack;
+
+/**
+ * Invert a binary tree.
+ */
 public class InvertBinaryTree226 {
     public TreeNode invertTree(TreeNode root) {
 
@@ -7,6 +12,24 @@ public class InvertBinaryTree226 {
         TreeNode left = root.left;
         root.left = invertTree(root.right);
         root.right = invertTree(left);
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop(), left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if (node.left != null)
+                stack.push(node);
+            if (node.right != null)
+                stack.push(node.right);
+        }
         return root;
     }
 }
