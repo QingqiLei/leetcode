d98e9b581835ac7917f3b5714c5976309c0e5b9b
bomb <qingqi_lei@icloud.com>
2018-10-18 23:42:14 +0800

    BFS  using Queue, first in first out, one layer by one layer

--- src/tree/easy/AverageofLevelsinBinaryTree637.java
+++ src/tree/easy/AverageofLevelsinBinaryTree637.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+/*
 
+ */
 public class AverageofLevelsinBinaryTree637 {
     public List<Double> averageOfLevels(TreeNode root){
         List<Double> res = new ArrayList<>();
