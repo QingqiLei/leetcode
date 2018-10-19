21769f985da5afea50c98f3224ea0dd53e100e6a
bomb <qingqi_lei@icloud.com>
2018-10-20 00:01:38 +0800

    the 2-th minimum node, the root is minimum in its tree

--- src/tree/easy/SecondMinimumNodeInaBinaryTree671.java
+++ src/tree/easy/SecondMinimumNodeInaBinaryTree671.java
@@ -1,5 +1,16 @@
 package tree.easy;
 
+/**
+ * Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes.
+ * <p>
+ * Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+ * <p>
+ * If no such second minimum value exists, output -1 instead.
+ * <p>
+ * <p>
+ * <p>
+ * means that every root is the minimum in its tree!
+ */
 public class SecondMinimumNodeInaBinaryTree671 {
     public int findSecondMinimumValue(TreeNode root) {
         if (root == null | root.left == null) return -1;
@@ -20,3 +31,5 @@ public class SecondMinimumNodeInaBinaryTree671 {
         else return right;
     }
 }
+
+
