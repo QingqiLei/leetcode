bd2efce5af938a462177cc82eebaaf0092555726
bomb <qingqi_lei@icloud.com>
2018-10-24 21:10:59 +0800

    using regex filter non-letter

---
+++ src/array/easy/ValidPalindrome125.java
@@ -0,0 +1,13 @@
+package array.easy;
+
+/**
+ *
+ */
+public class ValidPalindrome125 {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String revese = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+
+}
