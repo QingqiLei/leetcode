3d7724e98f79c64d14eec7bb66675037206d6d83
bomb <qingqi_lei@icloud.com>
2018-10-25 21:11:46 +0800

    easy solution

--- src/array/easy/BestTimetoBuyandSellStockII122.java
+++ src/array/easy/BestTimetoBuyandSellStockII122.java
@@ -1,8 +1,11 @@
 package array.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Say you have an array for which the ith element is the price of a given stock on day i.
- *
+ * <p>
  * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
  */
 public class BestTimetoBuyandSellStockII122 {
@@ -10,15 +13,25 @@ public class BestTimetoBuyandSellStockII122 {
 
         int total = 0;
         for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i + 1] > prices[i]) total += prices[i + 1] - prices[i];
+            if (prices[i + 1] > prices[i]) total += prices[i + 1] - prices[i]; // 只要prices[i+1] 大於 prices[i]
         }
-
         return total;
 
     }
 
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : nums) {
+            if (set.add(i) == false)
+                return false;
+        }
+        return true;
 
     }
 }
