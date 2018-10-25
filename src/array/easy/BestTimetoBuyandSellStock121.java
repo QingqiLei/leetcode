154a0bb8018cb5d5434fcb039614b45081c60509
bomb <qingqi_lei@icloud.com>
2018-10-25 21:08:30 +0800

    easy solution

--- src/array/easy/BestTimetoBuyandSellStock121.java
+++ src/array/easy/BestTimetoBuyandSellStock121.java
@@ -1,4 +1,4 @@
-package array.ilike;
+package array.easy;
 
 /**
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
@@ -9,9 +9,9 @@ public class BestTimetoBuyandSellStock121 {
         int soFarLow = prices[0];
         int profit = 0;
         for(int i = 0 ; i < prices.length; i++){
-            if(prices[i]< soFarLow){
+            if(prices[i]< soFarLow){   // 如果当前数字小于记录的最小的， 改变最小的
                 soFarLow = prices[i];
-            }else if(prices[i]>soFarLow){
+            }else if(prices[i]>soFarLow){  // 如果当前数字大于记录的最小的，求利润
                 profit= Math.max(profit,prices[i]-soFarLow);
             }
         }
