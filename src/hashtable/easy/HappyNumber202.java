f2d4f451b87404d41c509719cc8d0f0307c9d992
bomb <qingqi_lei@icloud.com>
2018-10-25 20:58:40 +0800

    happy

---
+++ src/hashtable/easy/HappyNumber202.java
@@ -0,0 +1,27 @@
+package hashtable.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber202 {
+    public boolean isHappy(int n) {
+        Set<Integer> temp = new HashSet<>();
+        int squareSum;
+        while (temp.add(n)) {
+            squareSum = 0;
+            while (n > 0) {
+                squareSum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+
+            n = squareSum;
+
+            if (squareSum == 1)
+                return true;
+            else
+                n = squareSum;
+        }
+        return false;
+    }
+
+}
