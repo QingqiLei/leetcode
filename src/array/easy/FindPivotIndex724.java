b877ac92a2db39fbb34e6276bcf94ad5959d501f
bomb <qingqi_lei@icloud.com>
2018-10-26 23:59:41 +0800

    easy solution, don't be lazy

--- src/array/easy/FindPivotIndex724.java
+++ src/array/easy/FindPivotIndex724.java
@@ -1,10 +1,14 @@
-package array.ilike;
+package array.easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Given an array of integers nums, write a method that returns the "pivot" index of this array.
- *
+ * <p>
  * We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
- *
+ * <p>
  * If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
  */
 public class FindPivotIndex724 {
@@ -12,10 +16,11 @@ public class FindPivotIndex724 {
     public static int pivotIndex(int[] nums) {
 
         int right = 0, left = 0;
-        for (int i = 0; i < nums.length; i++) right += nums[i];
+        for (int i = 0; i < nums.length; i++) right += nums[i]; //right == add up all numbers
+
 
         for (int i = 0; i < nums.length; i++) {
-            if (i != 0) left += nums[i - 1];
+            if (i != 0) left += nums[i - 1]; // left : add from left
             right = right - nums[i];
             if (left == right) return i;
         }
@@ -24,5 +29,8 @@ public class FindPivotIndex724 {
 
     public static void main(String[] args) {
         System.out.println(pivotIndex(new int[]{-1, -1, -1, 0, 1, 1}));
+        List<Integer> list = new ArrayList<>(5);
+
+
     }
 }
