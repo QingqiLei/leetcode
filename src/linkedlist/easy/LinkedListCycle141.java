d5417545091ad7958cb19cc8376be9bd82f9821b
bomb <qingqi_lei@icloud.com>
2018-10-30 22:12:25 +0800

    walker and runner solution

---
+++ src/linkedlist/easy/LinkedListCycle141.java
@@ -0,0 +1,17 @@
+package linkedlist.easy;
+
+public class LinkedListCycle141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode walker = head, runner = head;
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
+    }
+
+}
+
+
