3d36ecb81115b1145ba56afe8e47848dc477e438
bomb <qingqi_lei@icloud.com>
2018-10-31 23:32:49 +0800

    store each letter's last position

--- src/hashtable/easy/IsomorphicStrings205.java
+++ src/hashtable/easy/IsomorphicStrings205.java
@@ -7,6 +7,7 @@ public class IsomorphicStrings205 {
         for(int i = 0; i < s1.length(); i++){
             if(m[s1.charAt(i)] != m[s2.charAt(i)+256]) return false;
             m[s1.charAt(i)] = m[s2.charAt(i)+256] = i+1;
+
         }
         return true;
     }
