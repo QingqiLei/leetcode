e908256a37b6f91d98a27d711eec926e75f60240
chauncey <qingqi.lei@gmail.com>
2018-11-04 23:53:54 +0800

    priorityQueue, get the smallest by poll() and peek()

---
+++ src/bitoperation/easy/KthLargestinaStream703.java
@@ -0,0 +1,25 @@
+package bitoperation.easy;
+
+import java.util.PriorityQueue;
+// understanding PriorityQueue
+public class KthLargestinaStream703 {
+    final PriorityQueue<Integer> q;
+    final int k;
+    public KthLargestinaStream703(int k, int[] nums){
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for(int n: nums)
+            add(n);
+    }
+
+    public int add(int val){
+        if(q.size() < k)
+            q.offer(val);
+        else if(q.peek() < val){
+            q.poll();
+            q.offer(val);
+        }
+        return q.peek();
+
+    }
+}
