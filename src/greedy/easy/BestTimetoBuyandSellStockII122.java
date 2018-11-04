6756d4825088a015ca04773b5036ce1d32549b36
QingqiLei <qingqi_lei@icloud.com>
2018-11-04 13:57:06 +0800

    money change, easy solution

--- src/greedy/easy/BestTimetoBuyandSellStockII122.java
+++ src/greedy/easy/BestTimetoBuyandSellStockII122.java
@@ -1,4 +1,4 @@
-package array.easy;
+package greedy.easy;
 
 import java.util.HashSet;
 import java.util.Set;
---
+++ src/greedy/easy/LemonadeChange860.java
@@ -0,0 +1,25 @@
+package greedy.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LemonadeChange860 {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for(int i : bills){
+            if(i == 5)
+                five++;
+            else if(i == 10){
+                five++;
+                ten--;
+            }else if(ten > 0) {ten--;five--;}
+            else five -=3;
+            if(five <0 || ten < 0) return false;
+        }
+        return true;
+    }
+
+
+
+    }
