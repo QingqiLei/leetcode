4a751baa6de841c73c602f244ba4129fcf6d90f9
chauncey <qingqi.lei@gmail.com>
2018-11-04 23:41:04 +0800

    sum of two numbers

---
+++ src/bitoperation/easy/SumofTwoIntegers371.java
@@ -0,0 +1,15 @@
+package bitoperation.easy;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ */
+public class SumofTwoIntegers371 {
+    public int getSum(int a, int b) {
+//        return b == 0? a:getSum(a ^ b, (a & b) <<1);
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+    }
+}
