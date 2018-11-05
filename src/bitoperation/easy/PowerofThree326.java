a83c779f7022db364b2cf3ca3e1d934f14bc8800
chauncey <qingqi.lei@gmail.com>
2018-11-05 10:46:02 +0800

    power of three,

---
+++ src/bitoperation/easy/PowerofThree326.java
@@ -0,0 +1,11 @@
+package bitoperation.easy;
+
+public class PowerofThree326 {
+    public boolean isPowerOfThree(int n) {
+        if(n > 1){
+            while( n%3 == 0) n = n /3;
+
+        }
+        return n == 1;
+    }
+}
