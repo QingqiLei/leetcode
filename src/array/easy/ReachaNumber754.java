a6467025bef52243c478717111c898dbce72a2f5
chauncey <qingqi_lei@icloud.com>
2018-11-11 11:27:26 +0800

    tricky solution

---
+++ src/array/easy/ReachaNumber754.java
@@ -0,0 +1,19 @@
+package array.easy;
+
+public class ReachaNumber754 {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int sum = 0;
+        int steps = 0;
+        while(sum < target){
+            steps ++;
+            sum +=steps;
+        }
+
+        while((sum - target) %2 != 0){
+            steps++;
+            sum +=steps;
+        }
+        return steps;
+    }
+}
