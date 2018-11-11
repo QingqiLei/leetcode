b64f6ce2561e784b41c62287f342b2cc1faf9fd9
chauncey <qingqi_lei@icloud.com>
2018-11-11 15:34:53 +0800

    prime factors only include 2,3,5

---
+++ src/IntString/easy/UglyNumber263.java
@@ -0,0 +1,11 @@
+package IntString.easy;
+
+public class UglyNumber263 {
+    public boolean isUgly(int num) {
+        for(int i = 2; i < 6 && num > 0; i++)
+            while(num % i == 0) num /=i;
+            return num ==1;
+
+
+    }
+}
