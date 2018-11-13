99864b850f88c7db57fda111f0b4cfbaf5a5062f
chauncey <qingqi_lei@icloud.com>
2018-11-14 00:56:32 +0800

    !!!!!!!!!

---
+++ src/array/medium/threeSumWithMultiplicity923.java
@@ -0,0 +1,24 @@
+package array.medium;
+
+/**
+ * Given an integer array A, and an integer target, return the number of tuples i, j, k  such that i < j < k and A[i] + A[j] + A[k] == target.
+ */
+public class threeSumWithMultiplicity923 {  // j is bigger or equal to i
+    public int threeSumMulti(int[] A, int target) {
+        long[] c = new long[101];
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (int i = 0; i <= 100; i++)
+            for (int j = i; j <= 100; j++) {
+                int k = target - i - j;
+                if (k > 100 || k < 0) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if ( j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return (int)(res % (1e9 + 7));
+    }
+}
