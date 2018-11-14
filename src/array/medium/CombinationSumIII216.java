a8dfbad2e68d13bcf7622b61485600dde4275768
chauncey <qingqi_lei@icloud.com>
2018-11-14 22:55:22 +0800

    combination

--- src/array/medium/CombinationSumIII216.java
+++ src/array/medium/CombinationSumIII216.java
@@ -3,6 +3,9 @@ package array.medium;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+ */
 public class CombinationSumIII216 {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
