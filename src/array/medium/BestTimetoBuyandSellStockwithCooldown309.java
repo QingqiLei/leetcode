6925a9c0e4f0c0bc343f5b90352f6689814fe377
chauncey <qingqi_lei@icloud.com>
2018-11-15 23:57:15 +0800

    best time to buy and sell with cooldown

--- src/array/medium/BestTimetoBuyandSellStockwithCooldown309.java
+++ src/array/medium/BestTimetoBuyandSellStockwithCooldown309.java
@@ -2,6 +2,7 @@ package array.medium;
 
 /**
  * instead of T[i-1][k][0], we should actually use T[i-2][k][0] if we intend to buy on the i-th day. Everything else remains the same and the new recurrence relations are
+ *
  */
 public class BestTimetoBuyandSellStockwithCooldown309 {
     public int maxProfit(int[] prices) {
