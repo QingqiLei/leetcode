f8b00228efd8811bbae5d878329e87e78eb02ec7
chauncey <qingqi_lei@icloud.com>
2018-11-17 23:58:33 +0800

    rotate image

---
+++ src/array/medium/RotateImage48.java
@@ -0,0 +1,32 @@
+package array.medium;
+
+/**
+ * You are given an n x n 2D matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ */
+public class RotateImage48 {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[i].length ; j ++){
+                int temp = 0;
+
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+
+
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length/2 ; j ++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1 -j];
+                matrix[i][matrix.length -j-1] = temp;
+
+            }
+        }
+    }
+}
