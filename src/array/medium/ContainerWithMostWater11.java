e07ec2671b80bc00814628c62216f695cf81f1aa
chauncey <qingqi_lei@icloud.com>
2018-11-24 23:32:03 +0800

    easy and good solution

--- src/array/medium/ContainerWithMostWater11.java
+++ src/array/medium/ContainerWithMostWater11.java
@@ -3,7 +3,6 @@ package array.medium;
 public class ContainerWithMostWater11 {
     public int maxArea(int[] height) {
         int ans = 0;
-
         for (int left = 0, right = height.length - 1; left < right; ) {
             ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
 
