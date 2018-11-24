9a13ceca504046b1275664d9b4d006f3e5936c0b
chauncey <qingqi_lei@icloud.com>
2018-11-24 23:10:24 +0800

    group 1,2,3

---
+++ src/array/medium/SortColors75.java
@@ -0,0 +1,22 @@
+package array.medium;
+
+public class SortColors75 {
+    public void sortColors(int[] nums) {
+        int p0 = 0, p2 = nums.length -1, index = 0;
+        while(index <= p2){
+            if(nums[index]  == 0){
+                nums[index] = nums[p0];
+                nums[p0] = 0;
+                p0++;
+
+            }
+            else if(nums[index] ==2){
+                nums[index] =nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            index++;
+        }
+    }
+}
