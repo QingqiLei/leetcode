fa0d41c85b3313bd780681b959ca137afa4a1bef
chauncey <qingqi_lei@icloud.com>
2018-11-24 23:31:46 +0800

    good solution

---
+++ src/array/medium/ContainerWithMostWater11.java
@@ -0,0 +1,19 @@
+package array.medium;
+
+public class ContainerWithMostWater11 {
+    public int maxArea(int[] height) {
+        int ans = 0;
+
+        for (int left = 0, right = height.length - 1; left < right; ) {
+            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] > height[right]) right--;
+            else if (height[left] < height[right]) left++;
+            else {
+                right--;
+                left++;
+            }
+        }
+        return ans;
+    }
+}
