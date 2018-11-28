94e220370bed12d52f005e415bd631ce675aa5c8
chauncey <qingqi_lei@icloud.com>
2018-11-28 21:56:38 +0800

    build tree with inorder and postorder

--- src/tree/medium/ConstructBinaryTreefromInorderandPostorderTraversal106.java
+++ src/tree/medium/ConstructBinaryTreefromInorderandPostorderTraversal106.java
@@ -3,31 +3,30 @@ package tree.medium;
 import tree.TreeNode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /*
 The the basic idea is to take the last element in postorder array as the root, find the position of the root in the inorder array; then locate the range for left sub-tree and right sub-tree and do recursion. Use a HashMap to record the index of root in the inorder array.
  */
 public class ConstructBinaryTreefromInorderandPostorderTraversal106 {
+    Map<Integer, Integer> map = new HashMap<>();
+
     public TreeNode buildTreePostIn(int[] inorder, int[] postorder) {
-        if (inorder == null || postorder == null || inorder.length != postorder.length)
-            return null;
-        HashMap<Integer, Integer> hm = new HashMap<Integer,Integer>();
-        for (int i=0;i<inorder.length;++i)
-            hm.put(inorder[i], i);
-        return buildTreePostIn(inorder, 0, inorder.length-1, postorder, 0,
-                postorder.length-1,hm);
+        for(int i = 0; i < inorder.length;i++)
+            map.put(inorder[i],i );
+        return helper(0, inorder.length-1, postorder,postorder.length-1);
     }
 
 
-    private TreeNode buildTreePostIn(int[] inorder, int is, int ie, int[] postorder, int ps, int pe,
-                                     HashMap<Integer,Integer> hm){
-        if (ps>pe || is>ie) return null;
-        TreeNode root = new TreeNode(postorder[pe]);
-        int ri = hm.get(postorder[pe]);
-        TreeNode leftchild = buildTreePostIn(inorder, is, ri-1, postorder, ps, ps+ri-is-1, hm);
-        TreeNode rightchild = buildTreePostIn(inorder,ri+1, ie, postorder, ps+ri-is, pe-1, hm);
-        root.left = leftchild;
-        root.right = rightchild;
+    private TreeNode helper( int inStart, int inEnd, int[] postorder, int postEnd){
+        if(inStart > inEnd || postEnd < 0) return null;
+
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        int inIndex = map.get(postorder[postEnd]);
+
+        root.left = helper(inStart,inIndex-1 ,postorder ,postEnd - inEnd + inIndex -1 );
+        root.right = helper(inIndex +1, inEnd, postorder, postEnd - 1);
         return root;
+
     }
 }
