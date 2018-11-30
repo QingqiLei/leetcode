8ddf7230dbaea86eb7d9188b58650db339df8b0c
chauncey <qingqi_lei@icloud.com>
2018-11-30 23:36:25 +0800

    left < right and left <= right are equivalent for this problem

--- src/binarysearch/medium/FindFirstandLastPositionofElementinSortedArray34.java
+++ src/binarysearch/medium/FindFirstandLastPositionofElementinSortedArray34.java
@@ -3,16 +3,26 @@ package binarysearch.medium;
 public class FindFirstandLastPositionofElementinSortedArray34 {
     public int[] searchRange(int[] nums, int target) {
         int left = helper(nums, target);
-        if(nums[left] != target || left == nums.length) return new int[]{-1,-1};
-        return new int[]{left, helper(nums,target +1 )};
+        if(left == nums.length || nums[left] != target  ) return new int[]{-1,-1};
+        return new int[]{left, helper1(nums,target +1 )-1};
     }
 
     private int helper(int[] nums, int target){
-        int left = 0, right = nums.length;
+        int left = 0, right = nums.length ;
         while(left < right){    // not equal maybe better, cause when nums.length == 0, nums[0] is wrong.
             int mid = left + ((right - left) >>1);
             if(nums[mid] >=target) right = mid ;
-            else left =mid +1;                 // at last, left = right +1
+            else left =mid +1;
+        }
+        return left;
+    }
+
+    private int helper1(int[] nums, int target){  // right left, the last: right == left == mid, the last last:
+        int left = 0, right = nums.length-1 ;
+        while(left <= right){    // not equal maybe better, cause when nums.length == 0, nums[0] is wrong.
+            int mid = left + ((right - left) >>1);
+            if(nums[mid] >=target) right = mid -1;
+            else left =mid +1;
         }
         return left;
     }
