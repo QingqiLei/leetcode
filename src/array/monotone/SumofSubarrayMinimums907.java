b660db7e98c93754c3cb4e46484343cd62451cde
chauncey <qingqi_lei@icloud.com>
2018-12-05 22:32:43 +0800

    monotone array and stack

--- src/array/monotone/SumofSubarrayMinimums907.java
+++ src/array/monotone/SumofSubarrayMinimums907.java
@@ -1,5 +1,7 @@
 package array.monotone;
 
+import java.util.Stack;
+
 public class SumofSubarrayMinimums907 {
     /*
     [2, 9, 7, 8, 3, 4, 6, 1]
@@ -13,27 +15,54 @@ public class SumofSubarrayMinimums907 {
         int[] p = new int[A.length];
 
         p[0] = -1;
-        for(int i = 1; i< A.length; i++){
-            int left = i-1;
-            while(left >=0 && A[left] > A[i])          // ! one of there must contain =
+        for (int i = 1; i < A.length; i++) {
+            int left = i - 1;
+            while (left >= 0 && A[left] > A[i])          // ! one of there must contain =
                 left = p[left];
             p[i] = left;
         }
 
-        n[A.length-1] = A.length;
-        for(int i = A.length-2; i>=0;i--){
-            int right = i+1;
-            while(right < A.length && A[i] <= A[right]) // !
+        n[A.length - 1] = A.length;
+        for (int i = A.length - 2; i >= 0; i--) {
+            int right = i + 1;
+            while (right < A.length && A[i] <= A[right]) // !
                 right = n[right];
             n[i] = right;
         }
 
         int res = 0;
-        int modulo = (int)Math.pow(10, 9)+7;
-        for(int i = 0; i < A.length; i++){
-            res += A[i] * (i - p[i]) * (n[i]-i);
-            res %=modulo;
+        int modulo = (int) 1e9 + 7;
+        for (int i = 0; i < A.length; i++) {
+            res += A[i] * (i - p[i]) * (n[i] - i);
+            res %= modulo;
         }
         return res;
+    }
+
+    public int sumSubarrayMins1(int[] A) {
+        int res = 0, mod = (int) 1e9 + 7;
+        int[] left = new int[A.length], right = new int[A.length];
+        Stack<int[]> s1 = new Stack<>(), s2 = new Stack<>();
+
+        for (int i = 0; i < A.length; i++) {
+            int count = 1;
+            while (!s1.isEmpty() && s1.peek()[0] > A[i])
+                count += s1.pop()[1];
+            s1.push(new int[]{A[i], count});
+            left[i] = count;
+        }
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            int count = 1;
+            while (!s2.isEmpty() && s2.peek()[0] >= A[i])
+                count += s2.pop()[1];
+            s2.push(new int[]{A[i], count});
+            right[i] = count;
+        }
+
+        for (int i = 0; i < A.length; i++)
+            res = (res + A[i] * left[i] * right[i]) % mod;
+        return res;
+
     }
 }
