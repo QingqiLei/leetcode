845464ed70e90ec01b29a24107e30be4a86ac2b5
chauncey <qingqi_lei@icloud.com>
2018-12-12 23:48:37 +0800

    add explanation

--- src/array/combinations/CombinationSum39.java
+++ src/array/combinations/CombinationSum39.java
@@ -2,7 +2,10 @@ package array.combinations;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+the same repeated number may be chosen from nums unlimited number of times
+the sum the numbers is equal == target
+ */
 public class CombinationSum39 {
     public List<List<Integer>> combinationSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
@@ -15,8 +18,12 @@ public class CombinationSum39 {
         else if (target == 0) ans.add(new ArrayList<>(comb));
         else
             for (int i = start; i < candidates.length; i++) {
+                // not have to judge   if(i > start && candidates[i] == candidates[i-1]) continue;
+                // because the results can be repeated
                 comb.add(candidates[i]);
-                backtrack(ans, comb, candidates, i,target - candidates[i]); // not i + 1 because we can reuse same elements
+                // not i + 1, because we can reuse same elements,
+                // unlimited number of times
+                backtrack(ans, comb, candidates, i,target - candidates[i]);
 
                 comb.remove(comb.size() - 1);
             }
@@ -24,6 +31,6 @@ public class CombinationSum39 {
 
     public static void main(String[] args) {
         CombinationSum39 t = new CombinationSum39();
-        System.out.print(t.combinationSum(new int[]{2, 3, 6, 7}, 7));
+        System.out.print(t.combinationSum(new int[]{1,2,3}, 7));
     }
 }
