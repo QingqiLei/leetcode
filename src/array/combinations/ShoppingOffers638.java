f309afd9be603176b32cbcfb35539b96cde7a6ad
chauncey <qingqi_lei@icloud.com>
2018-12-13 00:24:22 +0800

    good, like combination

--- src/array/combinations/ShoppingOffers638.java
+++ src/array/combinations/ShoppingOffers638.java
@@ -5,36 +5,29 @@ import java.util.List;
 
 // try every possibilities
 public class ShoppingOffers638 {
-    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-        return helper(0, special, price, needs);
+    public int shoppingOffers(List<Integer> p, List<List<Integer>> s, List<Integer> n) {
+        return helper(0, s, p, n);
     }
-    private int helper(int pos, List<List<Integer>> special, List<Integer> price, List<Integer> needs) {
-        int minPrice=0, n=price.size();
 
-        //1. for individual prices
-        for(int i=0;i<n;i++) {
-            minPrice+=price.get(i)*needs.get(i);
-        }
+    private int helper(int pos, List<List<Integer>> sprcial, List<Integer> price, List<Integer> needs) {
+        int minPrice = 0;
+        for (int i = 0; i < price.size(); i++)
+            minPrice += needs.get(i) * price.get(i);
 
-        //2. try to get the offer
-        for(int s=pos;s<special.size();s++) {
-            List<Integer> offer=special.get(s);
-            List<Integer> tmp=new ArrayList<>(); // for new needs !
+        for (int s = pos; s < sprcial.size(); s++) {
+            List<Integer> offer = sprcial.get(s);
+            List<Integer> tmp = new ArrayList();
 
-            for(int i=0;i<n;i++) {
-                if(offer.get(i)>needs.get(i)) {
-                    tmp=null;
+            for (int i = 0; i < price.size(); i++) {
+                if (offer.get(i) > needs.get(i)) {
+                    tmp = null;
                     break;
-                }else{
-                    tmp.add(needs.get(i)-offer.get(i));
-                }
+                } else
+                    tmp.add(needs.get(i) - offer.get(i));
             }
-
-            if(tmp!=null) {
-                minPrice=Math.min(minPrice, offer.get(n)+helper(s,special,price,tmp));
-            }
+            if (tmp != null)
+                minPrice = Math.min(minPrice, offer.get( price.size()) + helper(s, sprcial, price, tmp));
         }
-
         return minPrice;
     }
 }
