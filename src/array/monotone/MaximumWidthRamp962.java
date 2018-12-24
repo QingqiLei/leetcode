58f10a9112491b2fec0d8ff087e16f1b822cd709
chauncey <qingqi_lei@icloud.com>
2018-12-24 23:38:25 +0800

    good

---
+++ src/array/monotone/MaximumWidthRamp962.java
@@ -0,0 +1,22 @@
+package array.monotone;
+
+import java.util.Stack;
+/*
+Given an array A of integers, a ramp is a tuple (i, j) for which i < j and A[i] <= A[j].  The width of such a ramp is j - i.
+ */
+public class MaximumWidthRamp962 {
+    public int maxWidthRamp(int[] A) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < A.length; i++)
+            if(stack.isEmpty() || A[stack.peek()]  > A[i])
+                stack.push(i);
+            int res = 0;
+            for(int i = A.length-1; i>=0; i--)
+                while (!stack.isEmpty() && A[stack.peek()] < A[i])
+                    res =Math.max(res, i - stack.pop());
+                return res;
+
+
+    }
+}
+
