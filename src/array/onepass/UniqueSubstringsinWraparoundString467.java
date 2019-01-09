82c77c099be3ecdaed521914865ebf4d1fb3b732
chauncey <qingqi_lei@icloud.com>
2019-01-09 09:00:12 +0800

    good one pass solution

---
+++ src/array/onepass/UniqueSubstringsinWraparoundString467.java
@@ -0,0 +1,22 @@
+package array.onepass;
+// find longest substring ends with each letter
+//
+public class UniqueSubstringsinWraparoundString467 {
+    public int findSubstringInWraproundString(String p) {
+        int[] array = new int[26];
+        int res = 0, cur = 0;
+        for (int i = 0; i < p.length(); i++) {
+            if (i == 0 || p.charAt(i) - p.charAt(i - 1) == 1
+                    || p.charAt(i - 1) == 'z' && p.charAt(i) == 'a') {
+                cur++;
+            } else {
+                cur = 1;
+            }
+            array[p.charAt(i) - 'a'] = Math.max(array[p.charAt(i) - 'a'], cur);
+
+        }
+        for (int i = 0; i < array.length; i++)
+            res += array[i];
+        return res;
+    }
+}
