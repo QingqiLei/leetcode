dc21017f113b49c549ea21d6e9a4a2793e316408
chauncey <qingqi_lei@icloud.com>
2019-01-13 18:11:09 +0800

    good

---
+++ src/array/medium/LongestPalindromicSubstring5.java
@@ -0,0 +1,27 @@
+package array.medium;
+
+public class LongestPalindromicSubstring5 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandFromCenter(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1; // 6-1 -1, from 2 to 5
+    }
+
+}
