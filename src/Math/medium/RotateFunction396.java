6e71f33405d72843f166a7b6da1243ae1003902d
chauncey <qingqi_lei@icloud.com>
2019-01-15 16:11:49 +0800

    good solution

---
+++ src/Math/medium/RotateFunction396.java
@@ -0,0 +1,20 @@
+package Math.medium;
+
+public class RotateFunction396 {
+    public int maxRotateFunction(int[] A) {
+        if (A.length == 0) return 0;
+        int sum = 0, iteration = 0, len = A.length;
+        for (int i = 0; i < len; i++) {
+            sum += A[i];
+            iteration += A[i] * i;
+
+        }
+        int max = iteration;
+        for (int i = 1; i < len; i++) {
+            iteration = iteration - sum + A[i - 1] * len;
+            max = Math.max(max, iteration);
+        }
+        return max;
+    }
+
+}
