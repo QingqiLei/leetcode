138fa15c942a5b2b219ca705619ce34ac8af75c3
chauncey <qingqi_lei@icloud.com>
2019-01-15 11:44:29 +0800

    good

---
+++ src/Math/medium/ReorderedPowerof2869.java
@@ -0,0 +1,19 @@
+package Math.medium;
+
+import java.util.Arrays;
+
+public class ReorderedPowerof2869 {
+    public boolean reorderedPowerOf2(int N) {
+        char[] a1 = String.valueOf(N).toCharArray();
+        Arrays.sort(a1);
+        String s1 = new String(a1);
+        for (int i = 0; i < 31; i++) {
+            char[] a2 = String.valueOf((int) (1 << i)).toCharArray();
+            Arrays.sort(a2);
+            String s2 = String.valueOf(a2);
+            if (s1.equals(s2)) ;
+        }
+        return false;
+    }
+
+}
