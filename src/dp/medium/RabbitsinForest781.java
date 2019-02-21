c197cf206c8f47835e718880370a87fb6a5b70ff
chauncey <qingqi_lei@icloud.com>
2019-02-22 05:40:17 +0800

    good

--- src/dp/medium/RabbitsinForest781.java
+++ src/dp/medium/RabbitsinForest781.java
@@ -4,11 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RabbitsinForest781 {
-    public int numRabbits(int[] answers) {
+    public static int numRabbits(int[] answers) {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i : answers) m.put(i, m.getOrDefault(i, 0) + 1);
         int res = 0;
-        for (int i : m.keySet()) res += (m.get(i) + i);
+        for (int i : m.keySet())
+            res += (m.get(i) + i) / (i + 1) * (i + 1);
         return res;
+    }
+
+    public static void main(String[] args) {
+        numRabbits(new int[]{10, 10, 10, 10, 10});
+//        System.out.println(Math.ceil);
     }
 }
