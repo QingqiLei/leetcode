c42583d39056d0460de7e5a2c29173a0dfbb5f86
chauncey <qingqi_lei@icloud.com>
2019-03-15 10:01:11 +0800

    good

--- src/Math/medium/MyPow50.java
+++ src/Math/medium/MyPow50.java
@@ -3,13 +3,14 @@ package Math.medium;
 public class MyPow50 {
     public double myPow(double x, int n) {
         if (n == 0) return 1;
+        if (n == Integer.MIN_VALUE)
+            return myPow(x * x, n / 2);
         if (n < 0) {
-            n = -n;
             x = 1 / x;
-            if (-n == Integer.MIN_VALUE)
-                return x * myPow(x, (n - 1));
+            n = -n;
         }
-        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+        if (n % 2 == 1) return x * myPow(x * x, n / 2);
+        else return myPow(x * x, n / 2);
     }
 
 
