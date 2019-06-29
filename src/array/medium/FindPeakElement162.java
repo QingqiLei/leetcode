15c67a6f599c580cf6d5a39a18b216cac6e611bc
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-06-29 15:48:38 +0800

    peak

---
+++ src/array/medium/FindPeakElement162.java
@@ -0,0 +1,13 @@
+package array.medium;
+
+public class FindPeakElement162 {
+    public int findPeakElement(int[] A) {
+        int left = 0, right = A.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] < A[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
