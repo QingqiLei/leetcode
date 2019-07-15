554dbd4643c979c9a6369027e94fa3f19d2fc71e
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-07-16 00:42:13 +0800

    overlap

--- src/array/medium/MyCalendarII731.java
+++ src/array/medium/MyCalendarII731.java
@@ -7,28 +7,16 @@ import java.util.TreeMap;
 
 public class MyCalendarII731 {
     private List<int[]> books = new ArrayList<>();
+    private List<int[]> overlap = new ArrayList<>();
 
     public boolean book(int start, int end) {
-        MyCalendar overlaps = new MyCalendar();
-        for(int[] b: books)
-            if(Math.max(b[0],start) < Math.min(b[1],end)) // overlap
-                if(!overlaps.book(Math.max(b[0],start), Math.min(b[1],end))) return false;
-        books.add(new int[]{start, end});
-        return true;
-    }
-
-    private static class MyCalendar {
-        List<int[]> books = new ArrayList<>();
-        private boolean book(int start, int end) {
-            for (int[] b : books)
-                if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-            books.add(new int[]{ start, end });
+        for(int[] lap : overlap)
+            if(end > lap[0] && lap[1] > start ) return false;
+            for(int[] book : books)
+                if(end  > book[0] && book[1] > start) overlap.add(new int[]{Math.max(book[0], start), Math.min(book[1], end)});
+                books.add(new int[]{start, end});
             return true;
-        }
     }
-
-
-
 
     private TreeMap<Integer, Integer> map  = new TreeMap<>();
     public boolean book1(int start, int end) {
@@ -38,18 +26,15 @@ public class MyCalendarII731 {
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
             count += entry.getValue();
             if(count > 2) {
-                map.put(start, map.get(start) - 1);
-                if(map.get(start) == 0) {
-                    map.remove(start);
-                }
-                map.put(end, map.get(end) + 1);
-                if(map.get(end) == 0) {
-                    map.remove(end);
-                }
+                if(map.get(start) == 1) map.remove(start);
+                else map.put(start, map.get(start)-1);
+                if(map.get(end) == 1) map.remove(end);
+                else map.put(end,map.get(end) +1);
                 return false;
             }
         }
         return true;
     }
+
 
 }
