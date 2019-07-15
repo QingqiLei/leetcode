07dfbc6fe07f6bed82d5f4bf7495824992e85626
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-07-16 00:46:50 +0800

    timeline +1 -1

---
+++ src/array/hard/MyCalendarIII732.java
@@ -0,0 +1,14 @@
+package array.hard;
+
+import java.util.TreeMap;
+
+public class MyCalendarIII732 {
+    TreeMap<Integer, Integer> timeLine = new TreeMap<>();
+    public int book(int start, int end) {
+        timeLine.put(start,timeLine.getOrDefault(start,0  ) +1);
+        timeLine.put(end, timeLine.getOrDefault(end,0 )-1);
+        int ongoing =  0,res = 0;
+        for(int k: timeLine.values()) res = Math.max(res, ongoing += k);
+        return res;
+    }
+}
