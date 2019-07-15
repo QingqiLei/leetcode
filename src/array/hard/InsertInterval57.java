26c60db06366ff6fb87000bf19f34caf3f3e5e85
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-07-16 01:29:21 +0800

    good

---
+++ src/array/hard/InsertInterval57.java
@@ -0,0 +1,26 @@
+package array.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval57 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[] newInterval1 = newInterval.clone();
+        List<int[]> res = new ArrayList<>();
+
+        for(int[] i: intervals){
+            if(newInterval1 == null || i[1] < newInterval1[0]) res.add(i);
+            else if(i[0] > newInterval1[1]){ // first element after overlapping
+                res.add(newInterval1);
+                res.add(i);
+                newInterval1 = null;
+            }
+            else{ // overlapping
+                newInterval1[0] = Math.min(newInterval1[0], i[0]);
+                newInterval1[1] = Math.max(newInterval1[1],i[1]);
+            }
+        }
+        if(newInterval1 != null) res.add(newInterval1);
+        return res.toArray(new int[res.size()][]);
+    }
+}
