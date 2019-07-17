fac98b960497886640e45b5b57bd2c2810e19f04
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-07-17 23:53:07 +0800

    good stack

--- src/stack/hard/LongestValidParentheses32.java
+++ src/stack/hard/LongestValidParentheses32.java
@@ -8,30 +8,19 @@ public class LongestValidParentheses32 {
         stack.push(-1);
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && stack.size() != 1 && s.charAt(stack.peek()) == '(') {
-                stack.pop();
-                result = Math.max(result, i - stack.peek());
-            } else stack.push(i);
+           if(s.charAt(i) == '(') stack.push(i);
+           else {
+               stack.pop();
+               if(stack.isEmpty()) stack.push(i);
+               else result = Math.max(result, i - stack.peek());
+           }
         }
         return result;
     }
 
-    public static int longestValidParentheses2(String s) {
-        int[] dp = new int[s.length()];
-        int result = 0;
-        int leftCount = 0;
-        for (int i = 0; i < s.length(); i++)
-            if (s.charAt(i) == '(') leftCount++;
-            else if (leftCount > 0) {
-                dp[i] = dp[i - 1] + 2;
-                dp[i] += (i - dp[i]) > 0 ? dp[i - dp[i]] : 0;
-                result = Math.max(result, dp[i]);
-                leftCount--;
-            }
-        return result;
-    }
 
-    public static void main(String[] args) {
-        System.out.println(longestValidParentheses2("()(())"));
-    }
+
+
+
+
 }
