88bc4101f5ccea925a6409402a0464dc2b718812
qingqi_lei@icloud.com <qingqi_lei@icloud.com>
2019-07-29 07:57:40 +0800

    good

--- src/array/monotone/NextGreaterElementI496.java
+++ src/array/monotone/NextGreaterElementI496.java
@@ -1,4 +1,4 @@
-package stack.easy;
+package array.monotone;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,14 +11,13 @@ public class NextGreaterElementI496 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num)
-                map.put(stack.pop(), num);
-            stack.push(num);
+        for(int i: nums2){
+            while(!stack.isEmpty() && stack.peek() < i)
+                map.put(stack.pop(),i);
+            stack.push(i);
         }
-        for (int i = 0; i < nums1.length; i++)
-            nums1[i] = map.getOrDefault(nums1[i], -1);
+        for(int i = 0; i < nums1.length; i++)
+            nums1[i] = map.getOrDefault(nums1[i],-1);
         return nums1;
     }
 }
